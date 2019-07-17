fe0d0cddabb685901409d17566cc7bd8cb05c451
DarrickAZ <zhwwhx@126.com>
2019-07-17 09:58:25 +0800

    ### add for brand.

---
+++ ly-common/src/main/java/com/leyou/common/pojo/PageResult.java
@@ -0,0 +1,10 @@
+package com.leyou.common.pojo;
+
+import java.util.List;
+
+public class PageResult<T> {
+    private Long total;// 总条数
+    private Long totalPage;// 总页数
+    private List<T> items;// 当前页数据
+
+}
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,11 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.pojo.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * 品牌mapper
+ */
+public interface BrandMapper extends Mapper<Brand> {
+}
